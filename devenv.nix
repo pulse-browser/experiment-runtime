2b3d8ec648e163aa59664e2f9f90912a8c56a979
trickypr <23250792+trickypr@users.noreply.github.com>
2024-06-17 17:45:31 +1000

    🐛 Fix packaging stuff + nixos

---
+++ devenv.nix
@@ -0,0 +1,124 @@
+{ pkgs, lib, config, inputs, stdenv, ... }:
+
+{
+  env.LIBCLANG_PATH = "${pkgs.libclang.lib}/lib";
+  
+  languages.c.enable = true;
+  languages.cplusplus.enable = true;
+  languages.rust.enable = true;
+  # https://devenv.sh/reference/options/#languagesrustchannel
+  languages.rust.channel = "stable";
+
+  packages = with pkgs; [
+    sccache
+    unzip
+
+    libllvm
+    libclang
+    llvm
+    alsa-lib
+    libpulseaudio
+    rust-cbindgen
+    libclang
+    gcc
+
+    libxkbcommon libdrm
+
+# build time
+ autoconf
+ cargo
+ dump_syms
+ makeWrapper
+ mimalloc
+ nodejs
+ perl
+ pkg-config
+ python3
+ rustc
+ rust-cbindgen
+ unzip
+ which
+
+# runtime
+ bzip2
+ dbus
+ dbus-glib
+ file
+ fontconfig
+ freetype
+ glib
+ gnum4
+ gtk3
+ icu
+ icu72
+  libGL
+  libGLU
+  libevent
+  libffi
+  libjpeg
+  libpng
+  libstartup_notification
+  libvpx
+  libwebp
+  nasm
+  nspr
+ nss_latest
+ pango
+ zip
+ zlib
+
+    bzip2
+    dbus
+    dbus-glib
+    file
+    fontconfig
+    freetype
+    glib
+    gtk3
+    libffi
+    libGL
+    libGLU
+    libevent
+    libjpeg
+    libpng
+    libstartup_notification
+    libvpx
+    libwebp
+    nasm
+    nspr
+    pango
+    perl
+    xorg.libX11
+    xorg.libXcursor
+    xorg.libXdamage
+    xorg.libXext
+    xorg.libXft
+    xorg.libXi
+    xorg.libXrender
+    xorg.libXt
+    xorg.libXtst
+    xorg.pixman
+    xorg.xorgproto
+    zip
+    zlib
+  ];
+
+  enterShell = ''
+    cxxLib=$( echo -n ${pkgs.gcc}/include/c++/* )
+    archLib=$cxxLib/$( ${pkgs.gcc}/bin/gcc -dumpmachine )
+
+    UNSET AS
+
+    cat - > mozconfig <<EOF
+    ac_add_options --disable-bootstrap
+    #ac_add_options --without-wasm-sandboxed-libraries # this may be needed
+    #mk_add_options AUTOCONF={autoconf213}/bin/autoconf
+    ac_add_options --with-libclang-path=${pkgs.libclang.lib}/lib
+    #ac_add_options --with-clang-path=${pkgs.clang}/bin/clang
+    #export BINDGEN_CFLAGS="-cxx-isystem $cxxLib -isystem $archLib"
+    #export LIBCLANG_PATH="${pkgs.libclang.lib}/lib"
+    #export CC="${pkgs.gcc}/bin/cc"
+    #export CXX="${pkgs.gcc}/bin/c++"
+    EOF
+  '';
+}
